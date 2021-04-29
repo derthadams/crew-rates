faa20c48814bf555e7db10cd1a8486b315409d8b
Derth Adams <derth@me.com>
2021-04-29 00:39:36 -0700

    Add dummy index view with site styling and POST logout link

--- rates/views.py
+++ rates/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.template import loader
 
 
 @login_required
 def index(request):
-    return HttpResponse("<p>Hello world, you're at the rates index.</p>"
-                        "<p><a href=\"/accounts/logout\">Log out</a></p>")
+    template = loader.get_template('rates/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
