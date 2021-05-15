8474ec21a92b058437941258bd585e27fc1ca8dc
Derth Adams <derth@me.com>
2021-05-15 13:24:54 -0700

    Change email alert so that it only fires on new raw rate reports (not when they are re-saved at the end of the approval process)

--- rates/signals.py
+++ rates/signals.py
@@ -6,16 +6,17 @@ from django.core.mail import send_mail
 
 @receiver(post_save, sender=RawRateReport)
 def new_rate_report_alert(sender, instance, **kwargs):
-    super_users = User.objects.filter(is_superuser=True)
-    emails = map(lambda x: x.email, super_users)
-    send_mail(
-        f'[crewrates.org alert] New rate report for '
-        f'{instance.job_title_name} on {instance.show_title} '
-        f'S{instance.season_number}',
-        f'New rate report for {instance.job_title_name} '
-        f'on {instance.show_title} S{instance.season_number}: '
-        f'${instance.hourly}/hr for {instance.guarantee} hours',
-        'alert@crewrates.org',
-        emails,
-        fail_silently=False
-    )
+    if not instance.approved:
+        super_users = User.objects.filter(is_superuser=True)
+        emails = map(lambda x: x.email, super_users)
+        send_mail(
+            f'[crewrates.org alert] New rate report for '
+            f'{instance.job_title_name} on {instance.show_title} '
+            f'S{instance.season_number}',
+            f'New rate report for {instance.job_title_name} '
+            f'on {instance.show_title} S{instance.season_number}: '
+            f'${instance.hourly}/hr for {instance.guarantee} hours',
+            'alert@crewrates.org',
+            emails,
+            fail_silently=False
+        )
