e9a353966a8cbcd5d1ca104937989e9603ac3ba6
Derth Adams <derth@me.com>
2021-05-14 19:41:53 -0700

    Add approve_raw_rate_report admin action

--- rates/admin.py
+++ rates/admin.py
@@ -71,3 +71,111 @@ admin.site.register(Location, LocationAdmin)
 admin.site.register(Season, SeasonAdmin)
 admin.site.register(RawRateReport)
 admin.site.register(RateReport)
+
+
+@admin.action(description="Approve raw rate report")
+def approve_raw_rate_report(modeladmin, request, queryset):
+    for raw_report in queryset:
+        if raw_report.job_title_id == -1:
+            job_title = JobTitle.objects.get_or_create(
+                title=raw_report.job_title_name)
+            job_title_id = job_title.pk
+        else:
+            job_title_id = raw_report.job_title_id
+
+        if raw_report.show_id == -1:
+            show = Show.objects.get_or_create(
+                title=raw_report.show_title)
+            show_id = show.pk
+        else:
+            show_id = raw_report.show_id
+
+        if raw_report.company_id == -1:
+            company = Company.objects.get_or_create(
+                name=raw_report.company_name)
+            company_id = company.pk
+        else:
+            company_id = raw_report.company_id
+
+        if raw_report.network_id == -1:
+            network = Network.objects.get_or_create(
+                name=raw_report.network_name)
+            network_id = network.pk
+        else:
+            network_id = raw_report.network_id
+
+        if raw_report.locations:
+            locations = set()
+            scopes = set()
+            for location in raw_report.locations['locations']:
+                location_object = make_location_object(location.scopes[0])
+                locations.add(location_object.id)
+                scopes.add(location_object.id)
+
+                for i in range(1, len(location.scopes)):
+                    scope_object = make_location_object(location.scopes[i])
+                    scopes.add(scope_object)
+            locations = list(locations)
+            scopes = list(scopes)
+        else:
+            locations = []
+            scopes = []
+
+        seasons = Season.objects.filter(
+            show__id=show_id,
+            number=raw_report.season_number
+        )
+        if len(seasons):
+            season = seasons[0]
+
+            if not season.start_date and not season.end_date:
+                season.start_date = raw_report.start_date
+                season.end_date = raw_report.end_date
+
+            season.company.add(company_id)
+
+            if not season.network:
+                season.network_id = network_id
+
+            season.locations.add(*locations)
+            season.scopes.add(*scopes)
+
+            if not season.genre:
+                season.genre = raw_report.genre
+
+            season.save()
+
+        else:
+            season = Season.objects.create(
+                show_id=show_id,
+                number=raw_report.season_number,
+                start_date=raw_report.start_date,
+                end_date=raw_report.end_date,
+                union=raw_report.union,
+                company_id=company_id,
+                network_id=network_id,
+                genre=raw_report.genre
+            )
+
+            season.locations.add(*locations)
+            season.scopes.add(*scopes)
+
+        RateReport.objects.create(
+            user=raw_report.user,
+            job_title_id=job_title_id,
+            hourly=raw_report.hourly,
+            guarantee=raw_report.guarantee,
+            season=season,
+            raw_report=raw_report
+        )
+
+        raw_report.approved = True
+
+
+def make_location_object(location_dict):
+    return Location.objects.get_or_create(
+        display_name=location_dict.display_name,
+        long_name=location_dict.long_name,
+        short_name=location_dict.short_name,
+        type=location_dict.type
+    )
