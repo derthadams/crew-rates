5e99dae89c7f1dfbcc7e923ad112af3005141bd3
Derth Adams <derth@me.com>
2021-05-15 13:18:37 -0700

    Change email alert on new rate report to include hourly and guarantee

--- rates/signals.py
+++ rates/signals.py
@@ -9,9 +9,12 @@ def new_rate_report_alert(sender, instance, **kwargs):
     super_users = User.objects.filter(is_superuser=True)
     emails = map(lambda x: x.email, super_users)
     send_mail(
-        '[crewrates.org alert] New rate report',
-        f'New rate report for {instance.job_title_name} on {instance.show_title} '
+        f'[crewrates.org alert] New rate report for '
+        f'{instance.job_title_name} on {instance.show_title} '
         f'S{instance.season_number}',
+        f'New rate report for {instance.job_title_name} '
+        f'on {instance.show_title} S{instance.season_number}: '
+        f'${instance.hourly}/hr for {instance.guarantee} hours',
         'alert@crewrates.org',
         emails,
         fail_silently=False
