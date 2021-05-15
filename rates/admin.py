18f16a2e9508de8393ad13cdb696f91803301c49
Derth Adams <derth@me.com>
2021-05-14 17:38:36 -0700

    Make company, show, network, locations fields in Season admin view into select2 autocompletes

--- rates/admin.py
+++ rates/admin.py
@@ -33,12 +33,41 @@ class UserAdmin(BaseUserAdmin):
     filter_horizontal = ('groups', 'user_permissions',)
 
 
+class CompanyAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+
+class ShowAdmin(admin.ModelAdmin):
+    ordering = ['title']
+    search_fields = ['title']
+
+
+class NetworkAdmin(admin.ModelAdmin):
+    ordering=['name']
+    search_fields = ['name']
+
+
+class LocationAdmin(admin.ModelAdmin):
+    ordering = ['display_name']
+    search_fields = ['display_name']
+
+
+class SeasonAdmin(admin.ModelAdmin):
+    autocomplete_fields = [
+        'company',
+        'show',
+        'network',
+        'locations',
+    ]
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Company)
+admin.site.register(Company, CompanyAdmin)
 admin.site.register(JobTitle)
-admin.site.register(Show)
-admin.site.register(Network)
-admin.site.register(Location)
-admin.site.register(Season)
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Network, NetworkAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Season, SeasonAdmin)
 admin.site.register(RawRateReport)
 admin.site.register(RateReport)
