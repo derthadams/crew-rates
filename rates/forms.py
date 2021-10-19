fa5d374e39a2a11adbfb3a87b8609f8591b3e23f
Derth Adams <derth@me.com>
2021-10-18 18:41:40 -0700

    Add Select2 specific classes, genre and union fields to Add form

--- rates/forms.py
+++ rates/forms.py
@@ -19,21 +19,26 @@ class RawRateReportForm(ModelForm):
         widgets = {
             'user': HiddenInput(),
             'job_title': HiddenInput(),
-            'job_title_name': Select(attrs={'required': 'required'}),
+            'job_title_name': Select(attrs={'required': 'required',
+                                            'class': 'form-select'}),
             'hourly': HiddenInput(),
             'guarantee': HiddenInput(),
             'show': HiddenInput(),
             'show_title': Select(attrs={'required': 'required'}),
+            'genre': Select(attrs={'required': 'required'}),
+            'union': Select(attrs={'required': 'required'}),
             'network': HiddenInput(),
             'network_name': Select(attrs={'required': 'required'}),
             'start_date': DateInput(attrs={'type': 'date'}),
             'end_date': DateInput(attrs={'type': 'date'}),
-            'companies': SelectMultiple,
+            'companies': Select(attrs={'multiple': 'multiple'}),
             'locations': Select(attrs={'multiple': 'multiple'}),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
+            # pass
             self.fields[field].widget.attrs.update({'class': 'form-control',
                                                     'autocomplete': 'off'})
+            # self.fields[field].widget.attrs.update({'autocomplete': 'off'})
