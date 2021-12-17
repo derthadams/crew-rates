f883ea798c5bdf0581a81129ccabe3bbf29dabc1
Derth Adams <derth@me.com>
2021-12-16 23:25:11 -0800

    Delete POST method for AddRateView (POST will now be handled by AddRate APIView at add-rate-api url in API app)

--- rates/views.py
+++ rates/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
-from django.middleware.csrf import CsrfViewMiddleware
+from django.http import HttpResponse
 from django.template import loader
-from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views import View
 from .forms import RawRateReportForm
-
-import json
 
 
 @login_required
@@ -25,61 +21,8 @@ class AddRateView(View):
         form = RawRateReportForm()
         return render(request, 'rates/add_rate.html', {'form': form})
 
-    @method_decorator(login_required)
-    def post(self, request, *args, **kwargs):
-        # sample_data = json.dumps({
-        #     "job_title": 1,
-        #     "job_title_name": "Camera Operator",
-        #     "hourly": 54.5454,
-        #     "guarantee": 10,
-        #     "show": 1,
-        #     "show_title": "Project Roonway",
-        #     "season_number": 4,
-        #     "companies": {},
-        #     "network": 1,
-        #     "network_name": "Bravo",
-        #     "locations": {},
-        #     "start_date": "2021-01-01",
-        #     "end_date": "2021-02-01",
-        #     "union": "IA",
-        #     "genre": "RE",
-        # })
-        # print(request.body)
-        # print(f"request body type:{type(request.body)}")
-        # print(request.POST)
-        data = json.loads(request.body)
-        # data = request.body.decode('utf-8')
-        # data = json.loads(sample_data)
-        headers = dict(request.headers)
 
-        data['user'] = request.user.id
-        # data['user'] = 1
-
-        # print(json.dumps(data, indent=4))
-        # print(json.dumps(headers, indent=4))
-
-        # form = RawRateReportForm(data)
-        form = RawRateReportForm(data)
-
-        # print(form.is_bound)
-
-        if form.is_valid():
-            # print("Form valid")
-            form.save()
-            return HttpResponseRedirect(reverse('thanks'))
-        else:
-            # print("Form not valid")
-            # print(form.errors)
-            # return HttpResponse("Form not valid")
-            # return render(request, 'rates/add_rate.html', {'form': form})
-            context = {'form': form}
-            # print(f"context: \n{context}")
-            return render(request, 'rates/add_rate.html', context)
-
-    # TODO: When user submits form the data is transmitted and processed, but the page
-    #       does not redirect to the thanks url!!
-
-
+@login_required
 def thanks(request):
     template = loader.get_template('rates/thanks.html')
     context = {}
