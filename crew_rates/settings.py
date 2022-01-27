659dcbebbe975215d36daf3ec9b9897e291122ad
Derth Adams <derth@me.com>
2022-01-27 00:53:11 -0800

    Add frontend/static to STATICFILES_DIRS

--- crew_rates/settings.py
+++ crew_rates/settings.py
@@ -216,6 +216,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
