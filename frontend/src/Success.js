e0418629dd6bfef02c25b8479dfe3910c70bb71e
Derth Adams <derth@me.com>
2022-01-27 19:49:17 -0800

    Add message, "Add Another Rate" button to Success page

--- frontend/src/Success.js
+++ frontend/src/Success.js
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+// import { useNavigate } from "react-router-dom";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button"
+
+import { useStateMachine } from "little-state-machine";
+import {clearFormData} from "./UpdateFunctions";
 
 function Success(props) {
-    return (
-        <div>
-            <h1>Success</h1>
+    const { actions, state } = useStateMachine({clearFormData})
+    // const navigate = useNavigate();
 
+    useEffect(() => {
+        actions.clearFormData({});
+    }, []);
+
+    const handleAnotherRate = () => {
+        navigate(`/`)
+    }
+
+    return (
+        <div className="my-3">
+            <h1 className="display-1">Thanks!</h1>
+            <p>We appreciate you submitting your rate report and giving back to the community!</p>
+            <p>Your report will appear on the site within 48 hours.</p>
+            <Row className="mt-5">
+                <Col className="col-4"> </Col>
+                <Col className="col-4">
+                    <Row>
+                        <Button
+                            variant="success"
+                            onClick={handleAnotherRate}
+                        >Add Another Rate</Button>
+                    </Row>
+                </Col>
+                <Col className="col-4"> </Col>
+            </Row>
         </div>
     )
 }
