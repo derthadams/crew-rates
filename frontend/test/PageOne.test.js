7de8af44ba3a2099e888c3a0f1d12df147188744
Derth Adams <derth@me.com>
2022-01-29 15:02:47 -0800

    Change from node-sass to sass

---
+++ frontend/test/PageOne.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import PageOne from "../src/PageOne"
+
+test("Page one is rendering", () => {
+    render(<PageOne/>);
+
+    const showSelect = screen.getByLabelText('Show title');
+    expect(showSelect).toBeEnabled();
+})
