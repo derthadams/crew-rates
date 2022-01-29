ec0c42ca37fb721d0499c7b78c9fab3bbf90dfb2
Derth Adams <derth@me.com>
2022-01-29 15:15:37 -0800

    Add babel, eslint, jest config files

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+    const presets = [
+        ['@babel/preset-env', {targets: {node: true}, modules: 'auto'}],
+        ['@babel/preset-react', {runtime: 'automatic'}]
+        // '@babel/preset-flow'
+    ];
+    // const plugins = [
+    //     '@babel/plugin-transform-runtime',
+    // ];
+
+    /** this is just for minimal working purposes,
+     * for testing larger applications it is
+     * advisable to cache the transpiled modules in
+     * node_modules/.bin/.cache/@babel/register* */
+    api.cache(false);
+
+    return {
+        presets,
+        // plugins
+    };
+};
