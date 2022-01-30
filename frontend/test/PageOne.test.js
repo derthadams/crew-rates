bd890a5f30bcd8f0e9ed90aef656c9e26835f2d7
Derth Adams <derth@me.com>
2022-01-29 22:34:07 -0800

    Write first two tests - Survey(passes), PageOne(fails)

--- frontend/test/PageOne.test.js
+++ frontend/test/PageOne.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import PageOne from "../src/PageOne"
 
 test("Page one is rendering", () => {
---
+++ frontend/test/Survey.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import Survey from '../src/Survey'
+
+describe('Survey', () => {
+    test('survey is rendering', () => {
+        window.history.pushState({}, 'Add a rate', '/add-rate/')
+
+        document.body.innerHTML =
+            "<div>  <script id=\"genreOptions\" type=\"application/json\">" +
+            "[{\"value\": \"RE\", \"label\": \"Reality\"}, " +
+            "{\"value\": \"DO\", \"label\": \"Documentary\"}, " +
+            "{\"value\": \"GA\", \"label\": \"Game Show\"}, " +
+            "{\"value\": \"LI\", \"label\": \"Live Show\"}, " +
+            "{\"value\": \"TA\", \"label\": \"Talk Show\"}, " +
+            "{\"value\": \"JU\", \"label\": \"Judge Show\"}, " +
+            "{\"value\": \"OT\", \"label\": \"Other Unscripted\"}, " +
+            "{\"value\": \"SC\", \"label\": \"Scripted\"}]</script>\n" +
+            "  <script id=\"unionOptions\" type=\"application/json\">" +
+            "[{\"value\": \"NO\", \"label\": \"Non-Union\"}, " +
+            "{\"value\": \"IA\", \"label\": \"IATSE\"}, " +
+            "{\"value\": \"NA\", \"label\": \"NABET\"}]</script>\n" +
+            "  <script id=\"apiUrls\" type=\"application/json\">" +
+            "{\"autocomplete\": \"/api/autocomplete/\", " +
+            "\"details\": \"/api/details/\", " +
+            "\"shows\": \"/api/shows/\", " +
+            "\"companies\": \"/api/companies/\", " +
+            "\"networks\": \"/api/networks/\", " +
+            "\"job-titles\": \"/api/job-titles/\", " +
+            "\"add-rate-api\": \"/api/add-rate/\"}</script></div>"
+
+        render(<Survey/>);
+    })
+})
