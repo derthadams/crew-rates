7b9d74c8b43d960af5c36be1aa0bf5380c8410da
Derth Adams <derth@me.com>
2022-02-03 16:31:14 -0800

    Install Mock Service Worker and write handlers, responses, server for mocking API

---
+++ frontend/test/mocks/handlers.js
@@ -0,0 +1,40 @@
+import { rest } from "msw";
+import { pro, bur, end, dis, details } from "./responses";
+
+export const handlers = [
+    rest.get("/api/shows/", (req, res, ctx) => {
+        const q = req.url.searchParams.get("q");
+
+        if (q === "Pro") {
+            return res(ctx.json(pro));
+        }
+    }),
+    rest.get("api/companies/", (req, res, ctx) => {
+        const q = req.url.searchParams.get("q");
+
+        if (q === "End") {
+            return res(ctx.json(end));
+        }
+    }),
+    rest.get("api/networks/", (req, res, ctx) => {
+        const q = req.url.searchParams.get("q");
+
+        if (q === "Dis") {
+            return res(ctx.json(dis));
+        }
+    }),
+    rest.get("api/autocomplete/", (req, res, ctx) => {
+        const q = req.url.searchParams.get("q");
+
+        if (q === "Bur") {
+            return res(ctx.json(bur));
+        }
+    }),
+    rest.get("api/details/", (req, res, ctx) => {
+        const q = req.url.searchParams.get("q");
+
+        if (q === "ChIJlcUYKBWVwoAR1IofkK-RdzA") {
+            return res(ctx.json(details));
+        }
+    })
+];
---
+++ frontend/test/mocks/responses.js
@@ -0,0 +1,374 @@
+export const pro = [
+    {
+        value: "48a6f024-2aa1-4f29-8db0-de0454385a2c",
+        label: "Project Runway"
+    },
+    {
+        value: "729bd86a-7682-49ce-80b6-2abaa2b52733",
+        label: "Project Runway All Stars"
+    },
+    {
+        value: "4e54b27c-6374-4438-9c6b-a7642c51c095",
+        label: "Prop Culture"
+    },
+    {
+        value: "dba9eb69-ca1f-4040-8215-95635d3643fd",
+        label: "Project Greenlight"
+    }
+]
+
+export const bur = {
+    predictions: [
+        {
+            description: "Burbank, CA, USA",
+            matched_substrings: [
+                {
+                    length: 3,
+                    offset: 0
+                }
+            ],
+            place_id: "ChIJlcUYKBWVwoAR1IofkK-RdzA",
+            reference: "ChIJlcUYKBWVwoAR1IofkK-RdzA",
+            structured_formatting: {
+                main_text: "Burbank",
+                main_text_matched_substrings: [
+                    {
+                        length: 3,
+                        offset: 0
+                    }
+                ],
+                secondary_text: "CA, USA"
+            },
+            terms: [
+                {
+                    offset: 0,
+                    value: "Burbank"
+                },
+                {
+                    offset: 9,
+                    value: "CA"
+                },
+                {
+                    offset: 13,
+                    value: "USA"
+                }
+            ],
+            types: [
+                "locality",
+                "political",
+                "geocode"
+            ]
+        },
+        {
+            description: "Burkina Faso",
+            matched_substrings: [
+                {
+                    length: 3,
+                    offset: 0
+                }
+            ],
+            place_id: "ChIJm3Cm1SbKLQ4Rq22DRu0Kkyc",
+            reference: "ChIJm3Cm1SbKLQ4Rq22DRu0Kkyc",
+            structured_formatting: {
+                main_text: "Burkina Faso",
+                main_text_matched_substrings: [
+                    {
+                        length: 3,
+                        offset: 0
+                    }
+                ]
+            },
+            terms: [
+                {
+                    offset: 0,
+                    value: "Burkina Faso"
+                }
+            ],
+            types: [
+                "country",
+                "political",
+                "geocode"
+            ]
+        },
+        {
+            description: "Burlingame, CA, USA",
+            matched_substrings: [
+                {
+                    length: 3,
+                    offset: 0
+                }
+            ],
+            place_id: "ChIJWY6bVCV2j4ART48vUz76vAs",
+            reference: "ChIJWY6bVCV2j4ART48vUz76vAs",
+            structured_formatting: {
+                main_text: "Burlingame",
+                main_text_matched_substrings: [
+                    {
+                        length: 3,
+                        offset: 0
+                    }
+                ],
+                secondary_text: "CA, USA"
+            },
+            terms: [
+                {
+                    offset: 0,
+                    value: "Burlingame"
+                },
+                {
+                    offset: 12,
+                    value: "CA"
+                },
+                {
+                    offset: 16,
+                    value: "USA"
+                }
+            ],
+            types: [
+                "locality",
+                "political",
+                "geocode"
+            ]
+        },
+        {
+            description: "Burundi",
+            matched_substrings: [
+                {
+                    length: 3,
+                    offset: 0
+                }
+            ],
+            place_id: "ChIJW_FUNtNEwRkRjewxVg5NI7E",
+            reference: "ChIJW_FUNtNEwRkRjewxVg5NI7E",
+            structured_formatting: {
+                main_text: "Burundi",
+                main_text_matched_substrings: [
+                    {
+                        length: 3,
+                        offset: 0
+                    }
+                ]
+            },
+            terms: [
+                {
+                    offset: 0,
+                    value: "Burundi"
+                }
+            ],
+            types: [
+                "country",
+                "political",
+                "geocode"
+            ]
+        },
+        {
+            description: "Burma",
+            matched_substrings: [
+                {
+                    length: 3,
+                    offset: 0
+                }
+            ],
+            place_id: "ChIJBylOcadSVjAREStiHOQOe7o",
+            reference: "ChIJBylOcadSVjAREStiHOQOe7o",
+            structured_formatting: {
+                main_text: "Burma",
+                main_text_matched_substrings: [
+                    {
+                        length: 3,
+                        offset: 0
+                    }
+                ]
+            },
+            terms: [
+                {
+                    offset: 0,
+                    value: "Burma"
+                }
+            ],
+            types: [
+                "country",
+                "political",
+                "geocode"
+            ]
+        }
+    ],
+    status: "OK"
+}
+
+export const end = [
+    {
+        value: "caf6966d-a961-4a57-8872-39a4f51ce798",
+        label: "EndemolShine NorthAmerica"
+    }
+]
+
+export const dis = [
+    {
+        value: "94754701-a98c-47f1-b80d-778ebdb43665",
+        label: "Disney+"
+    },
+    {
+        value: "077a0972-7a1d-4a1a-a64e-7cd04c90a4e5",
+        label: "Discovery Channel"
+    },
+    {
+        value: "c6c18b05-496f-4e66-a087-18132717a28d",
+        label: "Discovery+"
+    }
+]
+
+export const details = {
+    html_attributions: [],
+    result: {
+        address_components: [
+            {
+                long_name: "Burbank",
+                short_name: "Burbank",
+                types: [
+                    "locality",
+                    "political"
+                ]
+            },
+            {
+                long_name: "Los Angeles County",
+                short_name: "Los Angeles County",
+                types: [
+                    "administrative_area_level_2",
+                    "political"
+                ]
+            },
+            {
+                long_name: "California",
+                short_name: "CA",
+                types: [
+                    "administrative_area_level_1",
+                    "political"
+                ]
+            },
+            {
+                long_name: "United States",
+                short_name: "US",
+                types: [
+                    "country",
+                    "political"
+                ]
+            }
+        ],
+        adr_address: "<span class=\"locality\">Burbank</span>, <span class=\"region\">CA</span>, <span class=\"country-name\">USA</span>",
+        formatted_address: "Burbank, CA, USA",
+        geometry: {
+            location: {
+                lat: 34.1808392,
+                lng: -118.3089661
+            },
+            viewport: {
+                northeast: {
+                    lat: 34.22165400370276,
+                    lng: -118.2801090743378
+                },
+                southwest: {
+                    lat: 34.14236694648998,
+                    lng: -118.3703130578605
+                }
+            }
+        },
+        icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/geocode-71.png",
+        icon_background_color: "#7B9EB0",
+        icon_mask_base_uri: "https://maps.gstatic.com/mapfiles/place_api/icons/v2/generic_pinlet",
+        name: "Burbank",
+        photos: [
+            {
+                height: 558,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/114326007716204883291\">Otto Nassar M.</a>"
+                ],
+                photo_reference: "Aap_uEA4iBm5eLX9RSMgFvv71AcV5gE1uP5dOPpzvlwsPLPcqxsKsMiFtNKDTYlsZlYdTRL2crVbECHEoezQcwWL4uRQxBHBhuwFaaaat2pH6BxlDh1wze65fkP35bNKMNPvewuyXiyHaxXGLSn9CtlGLHdcuFd96fjHcDxZzk6EYfNBX43q",
+                width: 992
+            },
+            {
+                height: 4032,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/106792652186508314065\">Akhilesh Joshi</a>"
+                ],
+                photo_reference: "Aap_uEDjxMUOt9mpQmvSwArrpaKg22KWtZz-iM9GoNyXgMcB0YB2JLXxv1eAbpfm2QbiQxwFYPVkblfBYu-0KDlhbzl5nv9B4fJTNCkvqW3gqOvINPh1mguAHzJn3jvfCPuHfDv7rctkPweVyKV8RWBEewj_8BUav1XxYuJFzOiaQQ4rql7z",
+                width: 3024
+            },
+            {
+                height: 1080,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/116867796149422386593\">Nick Cortner</a>"
+                ],
+                photo_reference: "Aap_uEBu1UKi_fiG1CYsZqvJKNNtT3OpDgH2iBmqyzKSoiZKWN63Had7RKWRXBS6E8keIATieAWHtS04mFImcfo05nl9dYYYEY8z99j_GXWEklhvXZKaiLhgI3eM_R1vAK_Oa8T5vhxDVurrkdiFr_oSAIAplJzl2GZTmHqUxepcI22KIVrb",
+                width: 1080
+            },
+            {
+                height: 2848,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/105723652367168900981\">Matt Smith</a>"
+                ],
+                photo_reference: "Aap_uECiamo32pHHNrl6LtVt1eQWGWH7OWAfQSxIKYoVE6mnAEz_dNFVp6Fa_w-G4XXK7Hq54Os-AfAEpsBrUDkYm7cWIh5jwOx8hIlMCoPZcQY7Mseju-a8hCDWE3JEsd1kaNUvTLKmicTKU_m0_84NKLACxexYZKEsriDbQmmzyZmr_qh1",
+                width: 4288
+            },
+            {
+                height: 7963,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/116869188863835438571\">Alexander Viduetsky</a>"
+                ],
+                photo_reference: "Aap_uEDGVRyfH0CPw2UxF0CRTRbYMz5VJFCpUyIJ8skeij6gzLtQNj7Q2wl4UTVfvjdkG76OO-Aq2um81NG_3hYVl2oMSqwsJYEwOs9fgFYjTL-peRbMVmi18FWXMK7QDoYWEOPWUvv6evkf92pMIZ5PoV-hVkJOenZKgh8Km6fXzTnLFfie",
+                width: 9528
+            },
+            {
+                height: 3024,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/105872625279662536449\">Alexander Westerman</a>"
+                ],
+                photo_reference: "Aap_uEA5ua34NS2-Zah8bZ24PJ_2s3Z3Vy5rqar62wECWVgrVikDh0v1l3HztJoXhT4w2s0rVDnsz2g_zCMUP4fEpeZUa_ZCbA9DvQLhkvDWPysRo2Xorc5eCCWB-aY-CRYcee84M1vcCEjO-ygeLWis0PEOk5ZnNe4LwrYXT0OIf2Qmfc_L",
+                width: 4032
+            },
+            {
+                height: 3024,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/117009232071212399755\">James Baumgarten</a>"
+                ],
+                photo_reference: "Aap_uEDaDAXjjOL4djniN8U-DouGMPv9TsKqv8JyE1ADH5_iEVdRhbB3h93P5UV29fPsG4zn4_rNZBG-oVSYkqX8JlVHLt25HnRt4JaTowvnjmhp8RJm1VE6301oEKTozxnIDU4l-yd8ouR8Lzg0yTaMhwHLvEQOn9VdH9gb7p-jyVR0Dx_U",
+                width: 4032
+            },
+            {
+                height: 3024,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/107119469250737363827\">Jack Kaufman</a>"
+                ],
+                photo_reference: "Aap_uED0rwjVm0McP-5-ii37-eQFfTfmkt4Bt11TzLEwh3CyjdS0I-pnC3HHv82K3ZcElUo7vpKsbL4zNosRhu7yzW73aTzSLeGLWsBNPshPzqxhZQIzbcwOghW9NZo_vqrs9myhBH46xi76K8ABgdIVfwIZ6NRl2ZSI0NkcDKrYVl4V6_77",
+                width: 4032
+            },
+            {
+                height: 3036,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/116915041441403037997\">Ezequiel Barragan</a>"
+                ],
+                photo_reference: "Aap_uEAY8gJRLB6djoPER7XcBig634NDS8Svu97frxNjTexqhFyGuJ5LDDcGjS6btBiG-olG_Hewg5SsN7xQuRxnPGvZ5PPNaCzRXRE7LfIzz9PDvBg2Fh-q7wRDW1XxN_SnXC9cCDL-WfYvSHAFYJW07o9eklmGtgdDStlyMjqCKZXYnSXV",
+                width: 4048
+            },
+            {
+                height: 3024,
+                html_attributions: [
+                    "<a href=\"https://maps.google.com/maps/contrib/104519374123741166865\">Diego Galtieri</a>"
+                ],
+                photo_reference: "Aap_uEALObd-Qm3Sv46FfUsnnw7aIdkn94POkF93x5WSVkZ2o_j9Kh6YnncyS9YHGIQJDHQ3osWlJPgyOevyiU-U1BSdsO4NlbadYvoE0dl0hqx-IVWEKia5VPlUkFkz2L3Q4sx1UUmaI_T5z8msh1zELfC0GLqIe9j46nfRGBPIHNa_EBMI",
+                width: 4032
+            }
+        ],
+        place_id: "ChIJlcUYKBWVwoAR1IofkK-RdzA",
+        reference: "ChIJlcUYKBWVwoAR1IofkK-RdzA",
+        types: [
+            "locality",
+            "political"
+        ],
+        url: "https://maps.google.com/?q=Burbank,+CA,+USA&ftid=0x80c295152818c595:0x307791af901f8ad4",
+        utc_offset: -480,
+        vicinity: "Burbank",
+        website: "http://www.ci.burbank.ca.us/"
+    },
+    status: "OK"
+}
