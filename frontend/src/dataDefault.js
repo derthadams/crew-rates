524a05c8689225e9c0db40f1d2a8929a356457e9
Derth Adams <derth@me.com>
2022-02-04 23:30:43 -0800

    Give dataDefault its own file

---
+++ frontend/src/dataDefault.js
@@ -0,0 +1,23 @@
+exports.dataDefault = {
+    locationDetails: {},
+    formData: {
+        show_title: '',
+        season_number: '',
+        companies: [],
+        network: '',
+        genre: '',
+        union: '',
+        locations: [],
+        start_date: '',
+        end_date: '',
+        job_title: '',
+        offered_guarantee: '',
+        offered_day_rate: '',
+        offered_hourly_rate: '',
+        negotiated: '',
+        increased: '',
+        final_guarantee: '',
+        final_day_rate: '',
+        final_hourly_rate: '',
+    }
+}
