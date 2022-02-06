4579d20e46bf8f45bff42733953f5c01f3ad197d
Derth Adams <derth@me.com>
2022-02-06 11:31:13 -0800

    Survey: import dataDefault from external file

--- frontend/src/Survey.js
+++ frontend/src/Survey.js
@@ -11,29 +11,7 @@ import PageThree from "./PageThree"
 import Success from "./Success"
 import Failure from "./Failure"
 
-const dataDefault = {
-    locationDetails: {},
-    formData: {
-        show_title: '',
-        season_number: '',
-        companies: [],
-        network: '',
-        genre: '',
-        union: '',
-        locations: [],
-        start_date: '',
-        end_date: '',
-        job_title: '',
-        offered_guarantee: '',
-        offered_day_rate: '',
-        offered_hourly_rate: '',
-        negotiated: '',
-        increased: '',
-        final_guarantee: '',
-        final_day_rate: '',
-        final_hourly_rate: '',
-    }
-}
+import { dataDefault } from "./dataDefault"
 
 createStore(dataDefault, {});
 
