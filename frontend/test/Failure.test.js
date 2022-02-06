ee7db17791abc8d1a40e37c47796212cdaea6b60
Derth Adams <derth@me.com>
2022-02-06 11:33:12 -0800

    Implement test for Failure page

---
+++ frontend/test/Failure.test.js
@@ -0,0 +1,54 @@
+import {
+    BRRender as render,
+    screen,
+} from "./test-utils/testing-library-utils";
+import "@testing-library/jest-dom";
+
+import { dataDefault as mockDataDefault } from "../src/dataDefault";
+
+import optionsScript from "./optionsScript";
+
+import { server } from "./mocks/server";
+import Failure from "../src/Failure";
+
+
+const mockedUsedNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: () => mockedUsedNavigate,
+}));
+
+jest.mock("little-state-machine", () => ({
+    useStateMachine: () => {
+        return {
+            actions: {
+                updateFormData: jest.fn(),
+                updateLocationDetails: jest.fn(),
+            },
+            state: mockDataDefault,
+        };
+    },
+    createStore: jest.fn(),
+}));
+
+beforeAll(() => server.listen());
+
+// Pass optionsScript to the document body
+beforeEach(() => {
+    document.body.innerHTML = optionsScript;
+});
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
+
+describe("Failure page", () => {
+    test('Failure page renders', () => {
+        render(<Failure />)
+        const failureHeader = screen.getByText(/failure/i)
+        expect(failureHeader).toBeInTheDocument();
+    })
+})
