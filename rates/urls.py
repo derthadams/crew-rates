9eecfca36f55b0fd8f54f749032e6ce8081d7323
Derth Adams <derth@me.com>
2022-02-06 19:43:34 -0800

    Implement ContactFormView and thanks() and route urls

--- rates/urls.py
+++ rates/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.discover, name='discover'),
     path(r'add-rate/', views.add_rate, name='add-rate'),
-    path(r'add-rate/<path:path>', views.add_rate, name='add-rate_with_path')
+    path(r'add-rate/<path:path>', views.add_rate, name='add-rate_with_path'),
+    path(r'contact/', views.ContactFormView.as_view(), name='contact'),
+    path(r'thanks/', views.thanks, name='thanks'),
 ]
--- rates/views.py
+++ rates/views.py
@@ -5,8 +5,10 @@ from django.template import loader
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.generic.edit import FormView
 
 from .forms import RawRateReportForm
+from .forms import ContactForm
 from .models import Season
 
 
@@ -43,4 +45,22 @@ def add_rate(request, path=''):
             'add-rate-api': reverse('add-rate-api')
         }
     }
+    return HttpResponse(template.render(context, request))
+
+
+class ContactFormView(FormView):
+    template_name = 'rates/contact.html'
+    form_class = ContactForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        form.send_email()
+        form.save()
+        form = ContactForm()
+        return super().form_valid(form)
+
+
+def thanks(request):
+    template = loader.get_template('rates/thanks.html')
+    context = {}
     return HttpResponse(template.render(context, request))
