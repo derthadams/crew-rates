eea30191e116bcbc32d39eaea38ad4bc7770a775
Derth Adams <derth@me.com>
2022-02-09 20:13:50 -0800

    Make raw_report foreign key on RateReport model blank=True so that admin form save works properly

--- rates/models.py
+++ rates/models.py
@@ -333,7 +333,8 @@ class RateReport(models.Model):
     raw_report = models.ForeignKey(
         RawRateReport,
         on_delete=models.SET_NULL,
-        null=True)
+        null=True,
+        blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
