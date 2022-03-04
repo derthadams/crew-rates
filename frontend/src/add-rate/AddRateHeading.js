1f0c1ee7ef25549e96e76e0b1f543fe903359e0a
Derth Adams <derth@me.com>
2022-03-03 17:58:26 -0800

    Increase margin at top of AddRateHeading

--- frontend/src/add-rate/AddRateHeading.js
+++ frontend/src/add-rate/AddRateHeading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AddRateHeading({ step }) {
     return (
-        <div className={"mb-3"}>
+        <div className={"mb-3 mt-2"}>
             <div className={"d-none d-sm-block"}>
             <h3 className="display-3">Add a rate</h3>
             <p>
@@ -10,11 +10,6 @@ export default function AddRateHeading({ step }) {
                 negotiate better deals.
             </p>
             </div>
-            {/*<div className={"d-flex mt-0 px-1"} style={{justifyContent: "space-between"}}>*/}
-            {/*    <span className={step === 1 ? "active-text": "greyed-text"}><small>1. Show info</small></span>*/}
-            {/*    <span className={step === 2 ? "active-text": "greyed-text"}><small>2. Job info</small></span>*/}
-            {/*    <span className={step === 3 ? "active-text": "greyed-text"}><small>3. Review</small></span>*/}
-            {/*</div>*/}
             <div className="">
                 <div className="progress">
                     <div className={step !== 1 ? "pb-inactive progress-bar" : "progress-bar"}
