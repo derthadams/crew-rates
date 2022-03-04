a366467a9870e6fa52061866d95dec6c0816cd60
Derth Adams <derth@me.com>
2022-03-04 00:35:52 -0800

    Update convertDate to handle case when dateString is not a valid date

--- frontend/src/common/convertDate.js
+++ frontend/src/common/convertDate.js
@@ -2,6 +2,9 @@ export default function convertDate(dateString, format = "long") {
     const date = new Date(dateString);
     const timezoneOffsetMs = date.getTimezoneOffset() * 60000
     date.setTime(date.getTime() + timezoneOffsetMs)
+    if(isNaN(date)) {
+        return "";
+    }
     if (format === "long") {
         return date.toLocaleDateString("en-US", {
             month: "long",
