a2c12d8980487d20071cda80a1a4515649740459
Derth Adams <derth@me.com>
2022-03-08 23:24:47 -0800

    Order job reports by final_daily so that DISTINCT clause will eliminate true duplicates

--- api/sql.py
+++ api/sql.py
@@ -7,7 +7,8 @@ season_list = """
             'name', sq.company_name,
             'uuid', sq.company_uuid)) as company_list,
             JSONB_AGG(DISTINCT sq.job_report) as job_reports 
-    FROM (SELECT rates_season.uuid as uuid, rates_season.start_date as start_date, rates_season.end_date as end_date, 
+    FROM (SELECT rates_season.uuid as uuid, rates_season.start_date as start_date, 
+                    rates_season.end_date as end_date, 
                     rates_season.title as title, rates_season.union as union_status, 
                     rates_season.genre as genre, rates_show.title as show_title, 
                     rates_show.uuid as show_uuid, rates_network.name as network_name,
@@ -22,7 +23,8 @@ season_list = """
                                 'daily', rates_ratereport.final_daily, 
                                 'hourly', rates_ratereport.final_hourly,
                                 'guarantee', rates_ratereport.final_guarantee, 
-                                'increase', rates_ratereport.percent_increase))) AS job_report 
+                                'increase', rates_ratereport.percent_increase) 
+                                ORDER BY rates_ratereport.final_daily)) AS job_report 
             FROM ((((((rates_season
             INNER JOIN rates_ratereport ON rates_season.id = rates_ratereport.season_id)
             INNER JOIN rates_jobtitle ON rates_jobtitle.id = rates_ratereport.job_title_id)
@@ -33,8 +35,10 @@ season_list = """
             WHERE (%s = 0 OR (%s = 1 AND rates_season.start_date >= %s)) 
                 AND (%s = 0 OR (%s = 1 AND rates_season.union = %s)) 
                 AND (%s = 0 OR(%s = 1 AND rates_season.genre = %s))
-GROUP BY rates_season.uuid, rates_season.start_date, rates_season.end_date, rates_season.title, rates_season.union, 
-            rates_season.genre, rates_jobtitle.id, rates_show.title, rates_show.uuid, 
-            rates_network.name, rates_network.uuid, rates_company.name, rates_company.uuid) as sq
-GROUP BY sq.uuid, sq.start_date, sq.end_date, sq.title, sq.union_status, sq.genre, sq.show_title, sq.show_uuid, 
-            sq.network_name, sq.network_uuid;"""
+GROUP BY rates_season.uuid, rates_season.start_date, rates_season.end_date, rates_season.title, 
+            rates_season.union, rates_season.genre, rates_jobtitle.id, rates_show.title, 
+            rates_show.uuid, rates_network.name, rates_network.uuid, rates_company.name, 
+            rates_company.uuid) as sq
+GROUP BY sq.uuid, sq.start_date, sq.end_date, sq.title, sq.union_status, sq.genre, sq.show_title, 
+            sq.show_uuid, sq.network_name, sq.network_uuid
+ORDER BY sq.start_date DESC;"""
