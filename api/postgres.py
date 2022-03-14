c3ae9c3febde99026ac5cbe653a0a6b03d5d78a8
Derth Adams <derth@me.com>
2022-03-13 20:29:33 -0700

    Write custom Median function based on postgres PERCENTILE_DISC When size of array is even, will pick the highest value from the lower half of the array

--- api/postgres.py
+++ api/postgres.py
@@ -1,5 +1,5 @@
 from django.contrib.postgres.fields import ArrayField
-from django.db.models import Subquery
+from django.db.models import Aggregate, DecimalField, Subquery
 from django.utils.functional import cached_property
 
 
@@ -12,3 +12,10 @@ class ArraySubquery(Subquery): # noqa
     @cached_property
     def output_field(self):
         return ArrayField(self.query.output_field)
+
+
+class Median(Aggregate): # noqa
+    function = 'PERCENTILE_DISC'
+    name = 'median'
+    output_field = DecimalField()
+    template = '%(function)s(0.5) WITHIN GROUP (ORDER BY %(expressions)s)'
