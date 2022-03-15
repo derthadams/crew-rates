2082583b500ebb616e595cd4ea9c085cb3d95668
Derth Adams <derth@me.com>
2022-03-14 20:12:16 -0700

    Take path to new relic init file out of .initialize() call in wsgi.py and put it in an environment variable

--- crew_rates/wsgi.py
+++ crew_rates/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 import newrelic.agent
-newrelic.agent.initialize('./newrelic.ini')
+newrelic.agent.initialize()
 import os # noqa
 
 from django.core.wsgi import get_wsgi_application # noqa
