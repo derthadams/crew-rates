429b655f37c09d9bd681ee442128087739912d41
Derth Adams <derth@me.com>
2022-03-14 18:46:11 -0700

    Added init for new relic

--- crew_rates/wsgi.py
+++ crew_rates/wsgi.py
@@ -6,10 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
+import newrelic.agent
+newrelic.agent.initialize('./newrelic.ini')
+import os # noqa
 
-import os
-
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application # noqa
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crew_rates.settings')
 
