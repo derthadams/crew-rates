945f32cd889f0cab7cf22d508c04f54de7fd45e7
Derth Adams <derth@me.com>
2022-03-25 12:34:29 -0700

    Delete commented-out code

--- frontend/src/discover/useObserver.js
+++ frontend/src/discover/useObserver.js
@@ -1,30 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 
-// export default function useObserver({ target, getReports, appendReports, nextURL }) {
-//     const [isIntersecting, setIsIntersecting] = useState(false);
-//     const [isLoading, setIsLoading] = useState(false);
-//
-//     const options = {};
-//
-//     const observer = new IntersectionObserver((entries,
-//                                                        observer) => {
-//         entries.forEach(entry => {
-//             if(entry.isIntersecting) {
-//                 console.log(entry.target);
-//                 setIsIntersecting(true);
-//                 setIsLoading(true);
-//             }
-//         });
-//     }, options);
-//
-//     if(target) {
-//         observer.observe(target.current);
-//     }
-//
-//
-//     return [isIntersecting, isLoading];
-// }
-
 export default function useObserver( targetRef ) {
     const [isLoading, setIsLoading] = useState(false);
     const observerRef = useRef(null);
