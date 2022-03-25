6a8e5956b684f48bd656180db21e320927e4a2de
Derth Adams <derth@me.com>
2022-03-25 15:55:41 -0700

    Split dataDefault into pageOneDefault and pageTwoDefault

--- frontend/src/add-rate/dataDefault.js
+++ frontend/src/add-rate/dataDefault.js
@@ -1,23 +1,31 @@
+export const pageOneDefault = {
+    show_title: '',
+    season_number: '',
+    companies: [],
+    network: '',
+    genre: '',
+    union: '',
+    locations: [],
+    start_date: '',
+    end_date: '',
+}
+
+export const pageTwoDefault = {
+    job_title: '',
+    offered_guarantee: '',
+    offered_day_rate: '',
+    offered_hourly_rate: '',
+    negotiated: '',
+    increased: '',
+    final_guarantee: '',
+    final_day_rate: '',
+    final_hourly_rate: '',
+}
+
 export const dataDefault = {
-        locationDetails: {},
-        formData: {
-            show_title: '',
-            season_number: '',
-            companies: [],
-            network: '',
-            genre: '',
-            union: '',
-            locations: [],
-            start_date: '',
-            end_date: '',
-            job_title: '',
-            offered_guarantee: '',
-            offered_day_rate: '',
-            offered_hourly_rate: '',
-            negotiated: '',
-            increased: '',
-            final_guarantee: '',
-            final_day_rate: '',
-            final_hourly_rate: '',
-        }
+    locationDetails: {},
+    formData: {
+        ...pageOneDefault,
+        ...pageTwoDefault
+    }
 }
